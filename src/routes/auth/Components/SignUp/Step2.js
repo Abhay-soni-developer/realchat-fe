7ce896be7c253d8b093b1b865c0296b964a775d5
Abhay Soni <abhaysoni@qainfotech.com>
2019-11-04 12:21:18 +0530

    conflic resolved after taking pull from dev1

--- src/routes/auth/Components/SignUp/Step2.js
+++ src/routes/auth/Components/SignUp/Step2.js
@@ -47,79 +47,6 @@ const styles = theme =>
 
 function Step2(props) {
 
-<<<<<<< HEAD
-    const { setValueOfField, classes } = props
-    const profilePhotoInput = useRef(null)
-    //mime types of accepted formats for profile photo
-    const allowedFormats = ['image/png', 'image/jpeg']
-
-    const onImageSelect = (e) => {
-        e.preventDefault()
-        let file = profilePhotoInput.current.files[0]
-        let fileReader = new FileReader()
-        fileReader.addEventListener('load', (e) => {
-            props.setValueOfField('profilePhoto', e.currentTarget.result)
-        })
-        fileReader.readAsDataURL(file)
-    }
-
-
-    const openImageSelectDialog = () => {
-        profilePhotoInput.current.click()
-    }
-
-    let progressCount = 0
-
-    return (
-        <>
-
-            <div className={classes.profilePhotoContainer}>
-                <input onChange={onImageSelect} type='file' className={classes.profilePhotoInput} ref={profilePhotoInput} accept={allowedFormats.join(' ')} multiple={false} name={'profilePhoto'} />
-                <Tooltip title={'Click to upload your profile photo'} placement="top">
-                    <div className={'profilePhoto'} onClick={openImageSelectDialog}>
-                        {props.values.profilePhoto
-                            ?
-                            (<img src={props.values.profilePhoto} />)
-                            :
-                            (<FontAwesomeIcon icon={['fas', 'user-circle']} size={'8x'} className={classes.profilePhotoDefaultIcon} />)}
-                    </div>
-                </Tooltip>
-            </div>
-
-
-            <DatePicker
-                name='dob'
-                label='Date of Birth'
-                fullWidth
-                required
-                value={props.values.dob}
-                onChange={(date) => { setValueOfField('dob', date) }}
-                InputProps={{
-                    disableUnderline: true,
-                    classes: { root: classes.dobFieldInput, focused: classes.dobFieldInputFocused }
-                }}
-            />
-
-            <div className={classes.genderSelectContainer}>
-                <FormLabel component="legend">Gender</FormLabel>
-                <RadioGroup
-                    aria-label="gender"
-                    name="gender"
-                    value={props.values.gender}
-                    onChange={props.handleChange} row
-                    classes={{ root: classes.genderRadioBtnContainer }}
-                >
-                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                    <FormControlLabel value="other" control={<Radio />} label="Other" />
-                </RadioGroup>
-            </div>
-
-            <div><Select/></div>
-
-        </>
-    )
-=======
   const { setValueOfField, classes } = props;
 
   useEffect(()=>{
@@ -169,7 +96,6 @@ function Step2(props) {
       </div>
     </div>
   );
->>>>>>> 74c83d172c14e801e5638972b28e8a9271ab5073
 }
 
 export default withStyles(styles)(Step2);
